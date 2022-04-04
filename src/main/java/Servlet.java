f0ffc8dd08de7e8364975c7d3d78e958366067a5
aleksMel <aleksandr.melkher@gmail.com>
2022-04-04 22:07:38 +0300

    Initial commit

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,11 @@
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+
+    }
+}
+
